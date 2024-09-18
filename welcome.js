cb4643a3820f328d025bcb33113b730f383bdd04
satya-vineeth <110622240+satya-vineeth@users.noreply.github.com>
2024-09-18 17:20:03 +0530

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,8 @@
+function resizeImages() {
+  const image1 = document.getElementById('image1');
+  const image2 = document.getElementById('image2');
+  image1.style.width = '150px';
+  image1.style.height = '150px';
+  image2.style.width = '150px';
+  image2.style.height = '150px';
+}
